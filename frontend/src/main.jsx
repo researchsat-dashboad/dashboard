ba181b16924f5861da125a2f135d8c342ff47e5c
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2024-07-16 11:04:38 +1000

    fix lint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
